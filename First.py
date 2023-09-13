1c7e26d31e896df91cc92e906db605c2931dd058
Edm1795 <aswitzer@ualberta.ca>
2023-09-12 22:10:47 -0600

    Update First.py Coverted to Class
    All tasks for a single automated need are combined into one class.

--- First.py
+++ First.py
@@ -1,56 +1,70 @@
 import pyautogui as ag
 
-def moveMouse(horiz,vert,time,click):
-    '''
-    Inputs: int: horizontal and vertical position where the mouse must end up
-    Time: int: mount of time to take to get pointer to its position
-    click: a str value of 'y' if a click is desired at final position
-    '''
-    ag.moveTo(horiz, vert, duration=time)
 
-    if click == 'y':
-        ag.click()
-    else:
+class TaskSet:
+    '''
+    Class for a single unified set of automated GUI movements and actions
+    '''
+    def __init__(self):
+
         pass
+    def moveMouse(self,horiz,vert,time,click):
+        '''
+        Inputs: int: horizontal and vertical position where the mouse must end up
+        Time: int: mount of time to take to get pointer to its position
+        click: a str value of 'y' if a click is desired at final position
+        '''
+        ag.moveTo(horiz, vert, duration=time)
 
-def click():
-    '''
-    Clicks the mouse
-    '''
-    ag.click()
+        if click == 'y':
+            ag.click()
+        else:
+            pass
 
-def pressKeys(holdKey,secondKey):
+    def click(self):
+        '''
+        Clicks the mouse
+        '''
+        ag.click()
 
-    '''
-    Double key press function: eg, ctrl + a
-    Inputs: holdKey: str key to hold down, eg: ctrl or shift
-    secondKey:  str second key to press eg, a
-    '''
-    ag.keyDown(holdKey)  # hold down the shift key
-    ag.press(secondKey)  # press the left arrow key
-    ag.keyUp(holdKey)
+    def pressKeys(self,holdKey,secondKey):
 
-def type(letters,enter):
-    '''
-    Types keyboard input to the cursor.
-    Inputs: letters: a sequence of strings to be typed
-    Enter: a str 'y' or 'no', if you want to press the enter key after inputing letters
-    '''
-    ag.write(letters)
+        '''
+        Double key press function: eg, ctrl + a
+        Inputs: holdKey: str key to hold down, eg: ctrl or shift
+        secondKey:  str second key to press eg, a
+        '''
+        ag.keyDown(holdKey)  # hold down the shift key
+        ag.press(secondKey)  # press the left arrow key
+        ag.keyUp(holdKey)
 
-    if enter == 'y':
-        ag.press('enter')
+    def type(self,letters,enter):
+        '''
+        Types keyboard input to the cursor.
+        Inputs: letters: a sequence of strings to be typed
+        Enter: a str 'y' or 'no', if you want to press the enter key after inputing letters
+        '''
+        ag.write(letters)
+
+        if enter == 'y':
+            ag.press('enter')
+
 
 # funcList = [moveMouse(706,1052,1,'y')]
 #
 # for func in funcList:
 #     func
 
-moveMouse(706,1052,1,'y')
-# moveMouse(706,1052,1,'y') # open Opera
-pressKeys('ctrl','t') # open new tab
-# moveMouse(886,79,1)  unnesecary, opera auto places cursor
-type('dayforcehcm.com','y') # Go to site
-moveMouse(1315,917,3,'y') # click login
-moveMouse(315,432,8,'y') # go to schedule
-moveMouse(985,241,6,'y') # open calendar
+def main():
+
+    taskSet1=TaskSet()
+
+    taskSet1.moveMouse(706,1052,1,'y')
+    taskSet1.pressKeys('ctrl', 't')  # open new tab
+    taskSet1.type('dayforcehcm.com', 'y')  # Go to site
+    taskSet1.moveMouse(1315, 917, 3, 'y')  # click login
+    taskSet1.moveMouse(315, 432, 8, 'y')  # go to schedule
+    taskSet1.moveMouse(985, 241, 6, 'y')  # open calendar
+
+main()
+
