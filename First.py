bacca688dc810f78b3b0afd8f3134fef8cc858c3
Edm1795 <aswitzer@ualberta.ca>
2023-09-25 16:46:30 -0600

    Update First.py Set up for mouse drag
    Handles times but not 8 pm or 9 pm

--- First.py
+++ First.py
@@ -31,23 +31,27 @@ class TaskSet:
         eg: 9 am is two time steps forward from basetime (8am) (two half hour units)
         '''
 
+        if desiredTime>0 and desiredTime<8:
+            desiredTime=desiredTime+12
+
         baseTime=8
 
         return (desiredTime-baseTime)*2
 
     def moveMouseNEW(self,time,shift,duration=1):
 
-        tBase=720 # base value for time which is 8 am (horizontal axis)
-        sBase=481 # base value for shift which is the top shift showing on screen (vertical axis)
+        #Vertical pixel difference between shifts: 50px
 
-        timeUnit=38 #number of pixels for a time unit (set at 30 mins;ie 38px = 30 mins of time)
+        shift = shift-1 # decrement by one to get correct results
+        shiftDiff=52 # pixel distance between shifts (adjust this if mouse is not accurante
+        tBase=524 # base value for time which is 8 am (horizontal axis)
+        sBase=351 # base value for shift which is the top shift showing on screen (vertical axis)
+
+        timeUnit=30 #number of pixels for a time unit (set at 30 mins;ie 38px = 30 mins of time)
 
         timeSteps=self.timeSteps(time) # number of time steps from base (8am) to desired time
 
-
-        if shift==1:
-            vert=sBase
-
+        vert=sBase+(shift*shiftDiff) # get shift pixel value multiply base value with number of shifts downwards on screen
         horiz=tBase+(timeSteps*timeUnit)
 
 
@@ -58,6 +62,27 @@ class TaskSet:
         Clicks the mouse
         '''
         ag.click()
+
+    def dragNEW(self,time,shift,button,duration=1):
+
+        if button=='l':
+            button='left'
+        if button=='r':
+            button='right'
+
+        shift = shift - 1  # decrement by one to get correct results
+        shiftDiff = 52  # pixel distance between shifts (adjust this if mouse is not accurante
+        tBase = 524  # base value for time which is 8 am (horizontal axis)
+        sBase = 351  # base value for shift which is the top shift showing on screen (vertical axis)
+
+        timeUnit = 30  # number of pixels for a time unit (set at 30 mins;ie 38px = 30 mins of time)
+
+        timeSteps = self.timeSteps(time)  # number of time steps from base (8am) to desired time
+
+        vert = sBase + (shift * shiftDiff)  # get shift pixel value multiply base value with number of shifts downwards on screen
+        horiz = tBase + (timeSteps * timeUnit)
+
+        ag.dragTo(horiz,vert, button=button,duration=duration)
 
     def drag(self,vert,horiz,duration,button):
 
@@ -88,41 +113,3 @@ class TaskSet:
 
         if enter == 'y':
             ag.press('enter')
-
-
-# funcList = [moveMouse(706,1052,1,'y')]
-#
-# for func in funcList:
-#     func
-
-def main():
-
-    taskSet1=TaskSet()
-    # taskSet1.moveMouse(967,91,1,'n')
-    # taskSet1.drag(820,91,2,'l')
-    # taskSet1.moveMouse(706,1052,1,'y')
-    # taskSet1.pressKeys('ctrl', 't')  # open new tab
-    # taskSet1.type('dayforcehcm.com', 'y')  # Go to site
-    # taskSet1.moveMouse(1315, 917, 3, 'y')  # click login
-    # taskSet1.moveMouse(315, 432, 8, 'y')  # go to schedule
-    # taskSet1.moveMouse(985, 241, 6, 'y')  # open calendar
-
-    # taskSet1.moveMouse(589,243,1,'y')
-    # for num in range(1,8):
-    #     taskSet1.pressKeys('ctrl',str(num))
-    #     time.sleep(1)
-    #     taskSet1.moveMouse(589, 243, 1, 'y')
-    # taskSet1.moveMouse(600,474,0.5,'y')
-    #
-    # taskSet1.moveMouse(655,474,0.5,'n')
-    # taskSet1.drag(767,474,2,'l')
-    #
-    # ag.dragTo(100, 200, 2,button='left')
-    # ag.moveTo(400,400)
-    # ag.drag(100, 200, 2,button='left')
-
-    taskSet1.moveMouseNEW(9,1)
-    taskSet1.moveMouseNEW(10,1)
-
-main()
-
