8962838bc31d9041f6308dbd224ede1e1056ec3f
Edm1795 <aswitzer@ualberta.ca>
2023-10-23 20:43:01 -0600

    Update Paging.py Adjusted Comments
    Note in this version you need Firefox, screen at 120% and bookmarks bar off

--- Paging.py
+++ Paging.py
@@ -1,16 +1,19 @@
-# Page Lists input (put screen to 120 %
+# Page Lists input (put screen to 120 %)
 
 import pyautogui as ag
+import time
 
 
 class TaskSet:
     '''
     Class for a single unified set of automated GUI movements and actions
     '''
+
     def __init__(self):
 
         pass
-    def moveMouse(self,horiz,vert,time,click):
+
+    def moveMouse(self, horiz, vert, time, click):
         '''
         Inputs: int: horizontal and vertical position where the mouse must end up
         Time: int: mount of time to take to get pointer to its position
@@ -29,15 +32,15 @@ class TaskSet:
         '''
         ag.click()
 
-    def drag(self,horiz,vert,duration,button):
+    def drag(self, horiz, vert, duration, button):
 
-        if button=='l':
-            button='left'
-        if button=='r':
-            button='right'
-        ag.dragTo(horiz,vert, button=button,duration=duration)
+        if button == 'l':
+            button = 'left'
+        if button == 'r':
+            button = 'right'
+        ag.dragTo(horiz, vert, button=button, duration=duration)
 
-    def pressKeys(self,holdKey,secondKey):
+    def pressKeys(self, holdKey, secondKey):
 
         '''
         Double key press function: eg, ctrl + a
@@ -48,7 +51,7 @@ class TaskSet:
         ag.press(secondKey)  # press the left arrow key
         ag.keyUp(holdKey)
 
-    def type(self,letters,enter):
+    def type(self, letters, enter):
         '''
         Types keyboard input to the cursor.
         Inputs: letters: a sequence of strings to be typed
@@ -59,11 +62,11 @@ class TaskSet:
         if enter == 'y':
             ag.press('enter')
 
+
 def main():
+    taskSet4 = TaskSet()
 
-    taskSet1=TaskSet()
-
-    # taskSet1.moveMouse(706,1052,1,'y')
+    # taskSet1.moveMouse(1142,505,1,'y')
     # taskSet1.pressKeys('ctrl', 't')  # open new tab
     # taskSet1.type('dayforcehcm.com', 'y')  # Go to site
     # taskSet1.moveMouse(1315, 917, 3, 'y')  # click login
@@ -73,28 +76,32 @@ def main():
     # for num in range(1,8): # switcher for days of the week from Sun to Sat.
     #     taskSet1.pressKeys('ctrl',str(num)) # hot key for changing days of week
 
-    print('set screen zoom to 120% for this program to work! You have 5 seconds. Press ctrl +)
-    
+
+    ##### Input Main Tasks for Paging #####
+
+    print('\nset screen zoom to 120% for this program to work! You have 5 seconds. Press ctrl +\nAlso turn off bookmarks bar')
+    taskSet1=TaskSet()
     time.sleep(5)
 
-    taskSet1.moveMouse(29,463,0.5,'y') # click activity button, left side pane
-    taskSet1.moveMouse(80,792,1,'y') # click 'Lists'
+    taskSet1.moveMouse(29, 463, 0.5, 'y')  # click activity button, left side pane
+    taskSet1.moveMouse(80, 792, 1, 'y')  # click 'Lists'
 
-    for num in range(1,8):
-        taskSet1.pressKeys('ctrl',str(num))
+    for num in range(1, 8):
+        taskSet1.pressKeys('ctrl', str(num))
 
-        taskSet1.moveMouse(630,406,1,'n') # go to 8 am top shift
-        taskSet1.drag(735,405,2,'l') # drag lists to 9:30 am
+        taskSet1.moveMouse(630, 406, 1, 'n')  # go to 8 am top shift
+        taskSet1.drag(735, 405, 2, 'l')  # drag lists to 9:30 am
 
-        taskSet1.moveMouse(737,405,1,'l') # shift previous 9:30 am end over slightly to start new block
-        taskSet1.drag(772,405,2,'l') # input Lists 9:30 am - 10 am
+        taskSet1.moveMouse(737, 405, 1, 'l')  # shift previous 9:30 am end over slightly to start new block
+        taskSet1.drag(772, 405, 2, 'l')  # input Lists 9:30 am - 10 am
 
         # Input 2 hour Lists block on 2nd shift
         taskSet1.moveMouse(701, 466, 1, 'n')
-        taskSet1.drag(844,468,2,'l')
+        taskSet1.drag(844, 468, 2, 'l')
 
         # Input 1 hour Lists on third shift\
         taskSet1.moveMouse(773, 528, 1, 'n')
         taskSet1.drag(844, 529, 2, 'l')
+
 
 main()
