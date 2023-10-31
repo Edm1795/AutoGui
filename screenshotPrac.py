d8277367a97ca192f1ec4644af340e8199a61936
Edm1795 <aswitzer@ualberta.ca>
2023-10-30 20:13:50 -0600

    Update screenshotPrac.py Updated to better explanation

--- screenshotPrac.py
+++ screenshotPrac.py
@@ -4,13 +4,13 @@ import pyautogui as ag
 
 
 
-im = ag.screenshot('Image.png') # takes screenshot and saves to same folder with title given in quotes
+# im = ag.screenshot('Image.png') # takes screenshot and saves to same folder with title given in quotes
 
 # ag.pixel((0, 0)) # Get colour value of pixel at given coordinates
 
 # Returns True if a match
 # ag.pixelMatchesColor(50, 200, (130, 135, 144)) # The first and second arguments are ints for x- and y-coord; the third arg is a tuple of three ints for the RGB color the screen pixel must match
 
-b = ag.locateOnScreen('Image.png')
+b = ag.locateOnScreen('Image.png') # find the image given to it (Image.png) inside the current screen showing on the screen
 
 print(b)
