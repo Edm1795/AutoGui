522ba72e100f4fdc47f8c27d3156744fa37f490a
Edm1795 <aswitzer@ualberta.ca>
2023-11-01 14:14:20 -0600

    Update DfQuickCheck.py Added element colour check on DF main screen

--- DfQuickCheck.py
+++ DfQuickCheck.py
@@ -1,5 +1,5 @@
+#opens a single instance of dayforce for quick check
 import pyautogui as ag
-
 import time
 
 class TaskSet:
@@ -93,99 +93,67 @@ class TaskSet:
         self.moveMouse(24,427,0.5,'y') #open activity pane
         self.moveMouse(66,813,0.5,'y') # select otf from list
 
-    def confirmElement(self, image, sector):
+
+    def confirmElement(self,image,sector):
 
         '''
         Confirms if a given element is present on the screen.
-        input: str of image to search for in the screen ('image.png')
+        input: image: str of image to search for in the screen ('image.png')
+        input: sector: str defining which sector of screen to search for desired element
         output: True if and when the element (the image sent in) is found
         '''
 
-        if sector == 'c':  # set screenshot region for small box in centre of the screen
+        if sector == 'c': # Centre Section: set screenshot region for small box in centre of the screen
             regValues = (756, 410, 400, 400)
+        if sector == 'cr':
+            regValues = (1000, 380, 500, 500)
 
         loop = True
         while loop:
 
-            if ag.locateOnScreen(image, region=regValues) == None:
+            if ag.locateOnScreen(image,region=regValues) == None:
+                continue
+            else:
+                loop = False
+
+        return True
+    def confirmElementCol(self,x,y,colour):
+
+        '''
+        Confirms an element is present by matching a colour expected to a colour on the screen
+        :param x: x coordinate of position of colour
+        :param y: y coordinate of position of colour
+        :param colour: a tuple (r,g,b) given in parantheses
+        :return: True once the colour is detected
+        '''
+
+        loop = True
+        while loop:
+            # use eyedroper in Firefox browser options to get colour then convert to rgb
+            if ag.pixelMatchesColor(x,y, colour) == False:
                 continue
             else:
                 loop = False
 
         return True
 
-# funcList = [moveMouse(706,1052,1,'y')]
-#
-# for func in funcList:
-#     func
+
+# Open last instance of Dayforce for current day
 
 def main():
 
-    print('\nEnsure bookmarks bar is on')
+    taskSet4=TaskSet()
 
-    taskSet1=TaskSet() # Scheduling for LA and Pages
-    taskSet2=TaskSet() # Sharepoint calendar
-    taskSet3=TaskSet() # Calendar for week of scheduling
-    taskSet4=TaskSet() # Dayforce for current day
-    taskSet5=TaskSet() # Open Email
 
-    taskSet1.moveMouse(605, 1056, 1, 'y')  # go to Firefox (5 position on taskbar)
-    time.sleep(2) # delay for first case scenerio; more time needed to open Firefox
-    taskSet1.moveMouse(305, 64, 0.2, 'y')  # click to focus address bar (incase not focused)
-    # taskSet1.pressKeys('ctrl', 't')  # open new tab (try to add delay here)
-    taskSet1.type('dayforcehcm.com', 'y')  # Go to site (consider adding click to addre. bar to ensure cursor)
-    taskSet1.moveMouse(1226, 640, 3, 'y')  # go to autofill user name; Firefox should auto pop this up
-    taskSet1.moveMouse(1226, 737, 0.2, 'y')  # go to Login
-
-    if taskSet1.confirmElement('SelectRole.png','c')==True:
-        taskSet1.moveMouse(915, 548, 0.5, 'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
-    taskSet1.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
-    taskSet1.moveMouse(1056, 337, 3, 'y')  # click schedule (main button to load sched)
-    taskSet1.moveMouse(246,223,6,'y') # click Filter button
-    taskSet1.moveMouse(376,261,0.5,'y') # click filter input bar
-    taskSet1.moveMouse(410,310,0.5,'y') # select LA
-    taskSet1.moveMouse(1629,301,0.2,'y') # click Apply button
-    taskSet1.moveMouse(1087, 188, 0.5, 'y')  # open calendar
-
-    # Open a second instance of Dayforce
-    taskSet1.pressKeys('ctrl', 't')  # open new tab (try to add delay here)
-    taskSet1.type('dayforcehcm.com', 'y')  # Go to site
-    taskSet1.moveMouse(1226, 640, 3, 'y')  # go to autofill user name; Firefox should auto pop this up
-    taskSet1.moveMouse(1226, 737, 0.2, 'y')  # go to Login
-    taskSet1.moveMouse(915, 548, 7, 'y')  # select Daily Scheduler (small box before sched loaded)
-    taskSet1.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
-    taskSet1.moveMouse(1056, 337, 2, 'y')  # click schedule (main button to load sched)
-    taskSet1.moveMouse(246, 223, 5, 'y')  # click Filter button
-    taskSet1.moveMouse(376, 261, 0.5, 'y')  # click filter input bar
-    taskSet1.moveMouse(396, 390, 0.5, 'y')  # select Page
-    taskSet1.moveMouse(1629, 301, 0.2, 'y')  # click Apply button
-    taskSet1.moveMouse(1087, 188, 0.5, 'y')  # open calendar
-
-    # Open Sharepoint
-    taskSet2.pressKeys('ctrl', 't')  # open new tab
-    taskSet2.moveMouse(53, 97, 1, 'y')  # click Sharepoint (on bookmarks tab)
-    taskSet2.moveMouse(21, 134, 5, 'y')  # click Sharepoint menu button (left side)
-    taskSet2.moveMouse(88, 447, 2, 'y')  # open calendar
-    taskSet2.moveMouse(702,20,1,'y')
-
-    # Open Sharepoint
-    taskSet3.pressKeys('ctrl', 't')  # open new tab
-    taskSet3.moveMouse(53, 97, 1, 'y')  # click Sharepoint
-    taskSet3.moveMouse(21, 134, 1, 'y')  # click outlook menu button (left side)
-    taskSet3.moveMouse(88, 447, 2, 'y')  # open calendar
-    taskSet3.moveMouse(572, 269, 1, 'y')  # open date selection calendar
-    taskSet3.moveMouse(926, 22, 1, 'y')  # open calendar
-
-    taskSet3.moveMouse(828, 22, 1, 'y')  # open calendar
-    taskSet3.drag(-1107,18,1,'l') # drag calendar to left screen
-
-    # Open last instance of Dayforce for current day
-    taskSet4.moveMouse(173, 68, 1, 'y')  # click on blank area of browser to focus the browser
+    taskSet4.moveMouse(173, 68, 0.5, 'y')  # click on blank area of browser to focus the browser
     taskSet4.pressKeys('ctrl', 't')  # open new tab (try to add delay here)
     taskSet4.type('dayforcehcm.com', 'y')  # Go to site
-    taskSet4.moveMouse(1226, 640, 3, 'y')  # go to autofill user name; Firefox should auto pop this up
+    # if taskSet4.confirmElement('Company.png','cr') == True: # monitor for when Select Role box displays then select Daily Scheduler (tiny radio button)
+    if taskSet4.confirmElementCol(665,575, (48,103,219))==True:
+        taskSet4.moveMouse(1226, 640, 0.25, 'y')  # go to autofill user name; Firefox should auto pop this up
     taskSet4.moveMouse(1226, 737, 0.2, 'y')  # go to Login
-    taskSet4.moveMouse(915, 548, 7, 'y')  # select Daily Scheduler (small box before sched loaded)
+    if taskSet4.confirmElement('SelectRole.png','c') == True: # monitor for when Select Role box displays then select Daily Scheduler (tiny radio button)
+        taskSet4.moveMouse(915, 548, 0.5,'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
     taskSet4.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
     taskSet4.moveMouse(1056, 337, 3, 'y')  # click schedule (main button to load sched)
     taskSet4.moveMouse(246, 223, 6, 'y')  # click Filter button
@@ -194,30 +162,6 @@ def main():
     taskSet4.moveMouse(1629, 301, 0.2, 'y')  # click Apply button
     taskSet4.moveMouse(1087, 188, 0.5, 'y')  # open calendar
 
-    # Open Email
-    taskSet5.pressKeys('ctrl', 't')  # open new tab
-    taskSet5.moveMouse(53, 97, 1, 'y')  # click Sharepoint (on bookmarks tab)
-    taskSet5.moveMouse(21, 134, 5, 'y')  # click Sharepoint menu button (left side)
-    taskSet5.moveMouse(83, 254, 2, 'y')  # open email (Outlook)
-
-
-
-
-
-
-
-
+    print(ag.pixelMatchesColor(215, 133, (56, 00, 00))) # use eyedroper in Firefox browser options to get colour then convert to rgb
 
 main()
-
-
-
-
-
-# moveMouse(1226,607,3,'y') # go to user name
-#
-# type('andswi','n') # Go to site
-#
-# moveMouse(1226,683,1,'y') # go to Pass
-
-
