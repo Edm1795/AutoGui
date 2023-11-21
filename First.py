646be68ff2cf95c06e6ba310eaf7ade283827656
Edm1795 <aswitzer@ualberta.ca>
2023-11-20 19:44:33 -0700

    Update First.py Added CheckforElem Class and updated calls
    All element checks are done from their own class, and all the calls in main() have been updated and work.

--- First.py
+++ First.py
@@ -50,7 +50,7 @@ class TaskSet:
     def type(self,letters,enter):
         '''
         Types keyboard input to the cursor.
-        Inputs: letters: a sequence of strings to be typed
+        Inputs: letters (str): a sequence of letters or numbers to be typed
         Enter: a str 'y' or 'no', if you want to press the enter key after inputing letters
         '''
         ag.write(letters)
@@ -93,7 +93,12 @@ class TaskSet:
         self.moveMouse(24,427,0.5,'y') #open activity pane
         self.moveMouse(66,813,0.5,'y') # select otf from list
 
-    def confirmElement(self, image, sector, topLeftx=0, topLefty=0, bottomRightx=0, bottomRighty=0):
+class CheckForElem:
+
+    def __init__(self):
+        pass
+
+    def confirmImage(self, image, sector, topLeftx=0, topLefty=0, bottomRightx=0, bottomRighty=0):
 
         '''
         Confirms if a given element is present on the screen.
@@ -105,7 +110,7 @@ class TaskSet:
 
         if sector == 'c':  # Centre Section: set screenshot region for small box in centre of the screen
             regValues = (756, 410, 400, 400)
-        if sector == 'cr':
+        if sector == 'cr': # Screenshot for centre right
             regValues = (1000, 380, 500, 500)
         if sector == 'n':  # If no sector is used, load in exact values of box to check for element
             regValues = (topLeftx, topLefty, bottomRightx, bottomRighty)
@@ -119,7 +124,8 @@ class TaskSet:
                 loop = False
 
         return True
-    def confirmElementCol(self,x,y,colour):
+
+    def confirmColour(self, x, y, colour):
 
         '''
         Confirms an element is present by matching a colour expected to a colour on the screen
@@ -132,13 +138,12 @@ class TaskSet:
         loop = True
         while loop:
             # use eyedroper in Firefox browser options to get colour then convert to rgb
-            if ag.pixelMatchesColor(x,y, colour) == False: # colour must be sent as tuple (r,g,b)
+            if ag.pixelMatchesColor(x, y, colour) == False:  # colour must be sent as tuple (r,g,b)
                 continue
             else:
                 loop = False
         time.sleep(0.1)
         return True
-
 
 # funcList = [moveMouse(706,1052,1,'y')]
 #
@@ -154,22 +159,22 @@ def main():
     taskSet3=TaskSet() # Calendar for week of scheduling
     taskSet4=TaskSet() # Dayforce for current day
     taskSet5=TaskSet() # Open Email
+    checkForElem = CheckForElem()
 
     taskSet1.moveMouse(605, 1056, 0.5, 'y')  # go to Firefox (5 position on taskbar)
     time.sleep(2) # delay for first case scenerio; more time needed to open Firefox
     taskSet1.moveMouse(305, 64, 0.2, 'y')  # click to focus address bar (incase not focused)
     # taskSet1.pressKeys('ctrl', 't')  # open new tab (try to add delay here)
     taskSet1.type('dayforcehcm.com', 'y')  # Go to site (consider adding click to addre. bar to ensure cursor)
-    if taskSet1.confirmElementCol(665, 575, (48, 103, 219)):
+    if checkForElem.confirmColour(665,575, (48,103,219)):
         taskSet1.moveMouse(1226, 640, 0.25, 'y')  # go to autofill user name; Firefox should auto pop this up
     taskSet1.moveMouse(1226, 737, 0.2, 'y')  # go to Login
-
-    if taskSet1.confirmElement('SelectRole.png','c'):
-        taskSet1.moveMouse(915, 548, 0.5, 'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
+    if checkForElem.confirmImage('SelectRole.png','c'):  # monitor for when Select Role box displays then select Daily Scheduler (tiny radio button)
+        taskSet1.moveMouse(915, 548, 0.2, 'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
     taskSet1.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
-    if taskSet4.confirmElement('Schedules.png', 'n', 1007, 370, 1113, 397):
-        taskSet1.moveMouse(1056, 337, 0.5, 'y')  # click schedule (main button to load sched)
-    if taskSet1.confirmElementCol(227, 223, (28, 68, 156)):  # Check for filter button by colour of icon
+    if checkForElem.confirmImage('Schedules.png','n',1007,370,1113,397):
+        taskSet1.moveMouse(1056, 337, 0.25, 'y')  # click schedule (main button to load sched)
+    if checkForElem.confirmColour(227, 223, (28, 68, 156)):  # Check for filter button by colour of icon
         taskSet1.moveMouse(246, 223, 0.2, 'y')  # click Filter button
     taskSet1.moveMouse(376,261,0.5,'y') # click filter input bar
     taskSet1.moveMouse(410,310,0.2,'y') # select LA
@@ -179,15 +184,15 @@ def main():
     # Open a second instance of Dayforce
     taskSet1.pressKeys('ctrl', 't')  # open new tab (try to add delay here)
     taskSet1.type('dayforcehcm.com', 'y')  # Go to site
-    if taskSet1.confirmElementCol(665, 575, (48, 103, 219)):
+    if checkForElem.confirmColour(665, 575, (48, 103, 219)):
         taskSet1.moveMouse(1226, 640, 0.25, 'y')  # go to autofill user name; Firefox should auto pop this up
     taskSet1.moveMouse(1226, 737, 0.2, 'y')  # go to Login
-    if taskSet1.confirmElement('SelectRole.png', 'c'):
-        taskSet1.moveMouse(915, 548, 0.5,'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
+    if checkForElem.confirmImage('SelectRole.png','c'):  # monitor for when Select Role box displays then select Daily Scheduler (tiny radio button)
+        taskSet1.moveMouse(915, 548, 0.2, 'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
     taskSet1.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
-    if taskSet4.confirmElement('Schedules.png', 'n', 1007, 370, 1113, 397):
-        taskSet1.moveMouse(1056, 337, 0.5, 'y')  # click schedule (main button to load sched)
-    if taskSet1.confirmElementCol(227, 223, (28, 68, 156)):  # Check for filter button by colour of icon
+    if checkForElem.confirmImage('Schedules.png', 'n', 1007, 370, 1113, 397):
+        taskSet1.moveMouse(1056, 337, 0.25, 'y')  # click schedule (main button to load sched)
+    if checkForElem.confirmColour(227, 223, (28, 68, 156)):  # Check for filter button by colour of icon
         taskSet1.moveMouse(246, 223, 0.2, 'y')  # click Filter button
     taskSet1.moveMouse(376, 261, 0.5, 'y')  # click filter input bar
     taskSet1.moveMouse(396, 390, 0.2, 'y')  # select Page
@@ -197,18 +202,18 @@ def main():
     # Open Sharepoint
     taskSet2.pressKeys('ctrl', 't')  # open new tab
     taskSet2.moveMouse(53, 97, 1, 'y')  # click Sharepoint (on bookmarks tab)
-    if taskSet2.confirmElementCol(215, 133, (56, 00, 00)):
+    if checkForElem.confirmColour(215, 133, (56, 00, 00)):
         taskSet2.moveMouse(21, 134, 0.5, 'y')  # click Sharepoint menu button (left side)
-    if taskSet2.confirmElement('Calendar.png', 'n', 25, 428, 65, 467):
+    if checkForElem.confirmImage('Calendar.png', 'n', 25, 428, 65, 467):
         taskSet2.moveMouse(88, 447, 0.5, 'y')  # open calendar
     taskSet2.moveMouse(702,20,1,'y')
 
     # Open Sharepoint
     taskSet3.pressKeys('ctrl', 't')  # open new tab
     taskSet3.moveMouse(53, 97, 1, 'y')  # click Sharepoint
-    if taskSet3.confirmElementCol(215, 133, (56, 00, 00)):
+    if checkForElem.confirmColour(215, 133, (56, 00, 00)):
         taskSet3.moveMouse(21, 134, 0.5, 'y')  # click Sharepoint menu button (left side)
-    if taskSet3.confirmElement('Calendar.png', 'n', 25, 428, 65, 467):
+    if checkForElem.confirmImage('Calendar.png', 'n', 25, 428, 65, 467):
         taskSet3.moveMouse(88, 447, 0.5, 'y')  # open calendar
     taskSet3.moveMouse(572, 269, 1, 'y')  # open date selection calendar
     taskSet3.moveMouse(926, 22, 1, 'y')  # open calendar
@@ -220,15 +225,15 @@ def main():
     taskSet4.moveMouse(173, 68, 1, 'y')  # click on blank area of browser to focus the browser
     taskSet4.pressKeys('ctrl', 't')  # open new tab (try to add delay here)
     taskSet4.type('dayforcehcm.com', 'y')  # Go to site
-    if taskSet1.confirmElementCol(665, 575, (48, 103, 219)):
+    if checkForElem.confirmColour(665, 575, (48, 103, 219)):
         taskSet4.moveMouse(1226, 640, 0.25, 'y')  # go to autofill user name; Firefox should auto pop this up
-    taskSet4.moveMouse(1226, 737, 0.2, 'y')  # go to Login
-    if taskSet4.confirmElement('SelectRole.png', 'c'):
-        taskSet4.moveMouse(915, 548, 0.5,'y')  # select Daily Scheduler (small box before sched
-    taskSet4.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
-    if taskSet4.confirmElement('Schedules.png', 'n', 1007, 370, 1113, 397):
-        taskSet1.moveMouse(1056, 337, 0.5, 'y')  # click schedule (main button to load sched)
-    if taskSet4.confirmElementCol(227, 223, (28, 68, 156)):  # Check for filter button by colour of icon
+    taskSet1.moveMouse(1226, 737, 0.2, 'y')  # go to Login
+    if checkForElem.confirmImage('SelectRole.png','c'):  # monitor for when Select Role box displays then select Daily Scheduler (tiny radio button)
+        taskSet4.moveMouse(915, 548, 0.2,'y')  # select Daily Scheduler (small box before sched loaded) !if this is missed the next function will not be available (shedule button)
+    taskSet1.moveMouse(1007, 660, 0.2, 'y')  # click next (on small box)
+    if checkForElem.confirmImage('Schedules.png', 'n', 1007, 370, 1113, 397):
+        taskSet4.moveMouse(1056, 337, 0.25, 'y')  # click schedule (main button to load sched)
+    if checkForElem.confirmColour(227, 223, (28, 68, 156)):  # Check for filter button by colour of icon
         taskSet4.moveMouse(246, 223, 0.2, 'y')  # click Filter button
     taskSet4.moveMouse(376, 261, 0.5, 'y')  # click filter input bar
     taskSet4.moveMouse(410, 310, 0.5, 'y')  # select LA
@@ -238,12 +243,12 @@ def main():
     # Open Email
     taskSet5.pressKeys('ctrl', 't')  # open new tab
     taskSet5.moveMouse(53, 97, 1, 'y')  # click Sharepoint (on bookmarks tab)
-    if taskSet5.confirmElementCol(215, 133, (56, 00, 00)):
+    if checkForElem.confirmColour(215, 133, (56, 00, 00)):
         taskSet5.moveMouse(21, 134, 0.5, 'y')  # click Sharepoint menu button (left side)
-    if taskSet5.confirmElement('Calendar.png', 'n', 25, 428, 65, 467):
+    if checkForElem.confirmImage('Calendar.png', 'n', 25, 428, 65, 467):
         taskSet5.moveMouse(83, 254, 0.5, 'y')  # open email (Outlook)
 
-
+    print('Complete.')
 
 main()
 
