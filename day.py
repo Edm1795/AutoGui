55a18bdde83d4d126da99dca151a0daa4e2712d3
Edm1795 <aswitzer@ualberta.ca>
2023-12-12 21:26:29 -0700

    Update day.py

--- day.py
+++ day.py
@@ -20,3 +20,22 @@ def dayFinder(day,date):
     
 for num in range(1,31):
     print('Date:',num,dayFinder(6,num))
+
+import datetime
+
+
+def week(year, month, day):
+    # Create a date object
+    dateObj = datetime.date(year, month, day)
+
+    # Get the ISO calendar week number and weekday
+    isoYear, isoWeekNumber, isoWeekday = dateObj.isocalendar()
+
+    # Calculate the week number within the month
+    firstDayOfMonth = datetime.date(year, month, 1)
+    firstWeekNumber = firstDayOfMonth.isocalendar()[1]
+    weekNuminMon = isoWeekNumber - firstWeekNumber + 1
+
+    return weekNuminMon
+
+print(week(2023,12,12))
