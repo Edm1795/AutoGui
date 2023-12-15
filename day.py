0cc42dd33d99880efb47fc5a14bbac7d5c4d6239
Edm1795 <aswitzer@ualberta.ca>
2023-12-15 15:02:54 -0700

    Update day.py

--- day.py
+++ day.py
@@ -1,41 +1,44 @@
-
-def dayFinder(day,date):
-    
-   
-    
-    if day == 1:
-        secondSunday=8
-        
-    else:
-        secondSunday=8-(day-1)
-        
-    
-    if date >= secondSunday:
-        week = date//7 + 1
-    else:
-        week = 1
-    
-    return week
-        
-    
-for num in range(1,31):
-    print('Date:',num,dayFinder(6,num))
-
+import pyautogui as ag
 import datetime
 
 
-def week(year, month, day):
-    # Create a date object
-    dateObj = datetime.date(year, month, day)
+def clickDate():
 
-    # Get the ISO calendar week number and weekday
-    isoYear, isoWeekNumber, isoWeekday = dateObj.isocalendar()
+    curDate=datetime.date.today() # get date object for current day; datetime.date.today() gives all three values ymd
+    day=curDate.weekday() # extract the weekday from the current date object as an int 0-6 for Mon-Sat.
 
-    # Calculate the week number within the month
-    firstDayOfMonth = datetime.date(year, month, 1)
-    firstWeekNumber = firstDayOfMonth.isocalendar()[1]
-    weekNuminMon = isoWeekNumber - firstWeekNumber + 1
+    def week(date=curDate.day, year=curDate.year, month=curDate.month):
+        # Create a date object
+        dateObj = datetime.date(year, month, date)
 
-    return weekNuminMon
+        # Get the ISO calendar week number and weekday
+        isoYear, isoWeekNumber, isoWeekday = dateObj.isocalendar()
 
-print(week(2023,12,12))
+        # Calculate the week number within the month
+        firstDayOfMonth = datetime.date(year, month, 1)
+        firstWeekNumber = firstDayOfMonth.isocalendar()[1]
+        weekNuminMon = isoWeekNumber - firstWeekNumber + 1
+
+        print(weekNuminMon)
+        return weekNuminMon
+
+    yDiff = 32 # number of pixels between adjacent rows (eg week 1 - week 2)
+    xDiff = 36 # number of pixels between immediately adjacent days of week (eg:mon-tues)
+    xDefault = 851 # 36 pixel difference
+    yDefault = 301
+
+    if day == 1:
+        x= xDefault
+    elif day == 2:
+        x = xDefault + (xDiff)
+    else:
+        x = xDefault + ((day-1)*xDiff)
+
+    if week()==1:
+        y = yDefault
+    else:
+        y = yDefault + (week() * yDiff)
+    
+    ag.moveTo(x,y)
+
+clickDate()
