9dbc60650ae2b6ba507c68a14af9d8ccee715302
Edm1795 <aswitzer@ualberta.ca>
2024-01-08 22:30:10 -0700

    Create TK.py New Version which uses TK and run DFQuickCheck as an import

---
+++ TK.py
@@ -0,0 +1,83 @@
+# colour picker crtl shift a, Type in colour
+import DFQuickCheck
+from tkinter import *
+from ctypes import windll  # used for fixing blurry fonts on win 10 and 11 (also  windll.shcore.SetProcessDpiAwareness(1))
+#from tkinter import ttk
+
+
+class MainWindow:
+
+    def __init__(self, master):
+
+        # Master Window
+        self.master = master
+        self.master.title('AutoGui Ver. 0.0')
+        self.master.geometry("+150+500")  # position of the window in the screen (200x300)
+        self.master.geometry("1000x400")  # set initial size of the root window (master) (1500x700);
+        # if not set, the frames will fill the master window
+        # self.master.attributes('-fullscreen', True)
+        screenWidth = self.master.winfo_screenwidth()
+        screenHeight = self.master.winfo_screenheight()
+
+        # Instantiate frames
+        self.frame0 = Frame(self.master, bd=5, padx=5, bg='#606266')  # Top long row
+        self.frame1 = Frame(self.master, bd=5, padx=5, bg='#2a2b2b')  # Side Column
+        self.frame2 = Frame(self.master, bd=5, padx=5, bg='#7E050C')  # Main frame
+
+        # Place frames
+        self.frame0.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        self.frame1.grid(row=1, column=0, columnspan=1, sticky="nsew")
+        self.frame2.grid(row=1, column=1, columnspan=1, sticky="nsew")
+
+        # configure weighting of frames
+        self.master.grid_columnconfigure(0,
+                                         weight=1)  # First int refers to column numberAllows frames to expand as master window expands; weight tells how much of the columns it takes
+        self.master.grid_columnconfigure(1, weight=7)  # weight gives 3 times as much column as the other columns
+        # self.master.grid_columnconfigure(2, weight=1)
+        self.master.grid_rowconfigure(1, weight=1)  # rowconfigure states: first row takes 1 parts of space
+
+        self.frame1.grid_propagate(0)  # When adding widgets maintain weighting of frames
+        self.frame2.grid_propagate(0)
+
+        frameWidth = 10  # Units are in characters not pixels
+
+        windll.shcore.SetProcessDpiAwareness(1)  # used for fixing blurry fonts on win 10 and 11
+
+
+        #  Entry widgets
+        self.entry = Entry(self.frame0, width=10)
+        self.entry.pack()
+
+        #self.photo = PhotoImage(file="Speedometer2.png")
+        #self.master.iconphoto(False, self.photo)
+
+
+        self.button = Button(self.frame1, text="Schedule", width=12, command=self.schedule)
+        self.button.pack()
+        #
+        # self.addItemButton = Button(self.frame1, text="Add Item", width=12, command=addItem)
+        # self.addItemButton.pack()
+        #
+        # self.clearFramesButton = Button(self.frame1, text="Clear Frames", width=12,
+        #                                 command=self.clearSpeedoFrame)  # Clears frame2, the speedometer frame of all widgets
+        # self.clearFramesButton.pack()
+        #
+        # self.showHistoryButton = Button(self.frame1, text="History", width=12,
+        #                                 command=showHistory)  # Clears frame2, the speedometer frame of all widgets
+        # self.showHistoryButton.pack()
+
+    def schedule(self):
+        DFQuickCheck.main()
+
+    def yes(self):
+        pass
+
+def main():
+    global mainWin  # Global mainWin so as to access the mainWin from functions which may need to call method
+    root = Tk()
+    mainWin = MainWindow(root)
+
+    root.mainloop()
+
+
+main()
